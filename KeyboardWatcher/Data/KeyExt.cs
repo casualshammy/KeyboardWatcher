871805a9ef324167726a3dc87e8bd850ca309653
casualshammy <axio@axio.name>
2023-11-18 13:05:25 +0000

    removed `ObservableHotkeyManager` nuget update

--- KeyboardWatcher/Data/KeyExt.cs
+++ KeyboardWatcher/Data/KeyExt.cs
@@ -1,100 +1,29 @@
-﻿using System;
-using System.Runtime.Serialization;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-namespace KeyboardWatcher.Data
+namespace KeyboardWatcher.Data;
+
+public readonly record struct KeyExt(Keys Key, bool Shift, bool Alt, bool Ctrl)
 {
-    [DataContract]
-    public class KeyExt : IEquatable<KeyExt>
+  private static KeysConverter p_keysConverter = new();
+
+  public Keys ConvertToKeys()
+  {
+    Keys key = Key;
+    if (Alt)
     {
-        // do not create smth like "public static readonly KeyExt None = new KeyExt(Keys.None);",
-        // this is a class, you will get a lot of bugs with references. Use "new KeyExt(Keys.None)"
-
-        private static KeysConverter p_keysConverter = new();
-        internal bool Pressed;
-        internal int PressedStartTime = 0;
-
-        [DataMember(Name = "Key")]
-        public readonly Keys Key;
-
-        [DataMember(Name = "Shift")]
-        public readonly bool Shift;
-
-        [DataMember(Name = "Alt")]
-        public readonly bool Alt;
-
-        [DataMember(Name = "Ctrl")]
-        public readonly bool Ctrl;
-
-        public KeyExt(Keys key, bool alt = false, bool shift = false, bool ctrl = false)
-        {
-            Key = key & ~Keys.Control & ~Keys.Shift & ~Keys.Alt;
-            Pressed = false;
-            Alt = alt;
-            Ctrl = ctrl;
-            Shift = shift;
-        }
-
-        public Keys ConvertToKeys()
-        {
-            Keys key = Key;
-            if (Alt)
-            {
-                key |= Keys.Alt;
-            }
-            if (Shift)
-            {
-                key |= Keys.Shift;
-            }
-            if (Ctrl)
-            {
-                key |= Keys.Control;
-            }
-            return key;
-        }
-
-        public override string ToString()
-        {
-            return p_keysConverter.ConvertToInvariantString(ConvertToKeys());
-        }
-
-
-        #region IEquatable<KeyExt>
-
-        public bool Equals(KeyExt _other)
-        {
-            if (ReferenceEquals(this, _other))
-                return true;
-            if (_other is null)
-                return false;
-            return Key == _other.Key && Alt == _other.Alt && Shift == _other.Shift && Ctrl == _other.Ctrl;
-        }
-
-        public override bool Equals(object other)
-        {
-            return Equals(other as KeyExt);
-        }
-
-        public static bool operator ==(KeyExt a, KeyExt b)
-        {
-            if (a is null)
-            {
-                return b is null;
-            }
-            return a.Equals(b);
-        }
-
-        public static bool operator !=(KeyExt a, KeyExt b)
-        {
-            return !(a == b);
-        }
-
-        public override int GetHashCode()
-        {
-            return (int)ConvertToKeys();
-        }
-
-        #endregion
-
+      key |= Keys.Alt;
     }
-}
+    if (Shift)
+    {
+      key |= Keys.Shift;
+    }
+    if (Ctrl)
+    {
+      key |= Keys.Control;
+    }
+    return key;
+  }
+
+  public override string ToString() => p_keysConverter.ConvertToInvariantString(ConvertToKeys()) ?? string.Empty;
+
+};
--- KeyboardWatcher/HotkeyManager.cs
+++ KeyboardWatcher/HotkeyManager.cs
@@ -7,121 +7,123 @@ using System.Threading;
 using System.Timers;
 using System.Windows.Forms;
 
-namespace KeyboardWatcher
+namespace KeyboardWatcher;
+
+public static class HotkeyManager
 {
-    public static class HotkeyManager
+  private static readonly System.Timers.Timer p_timer = new(50);
+  private static KeyExt[] p_uniqKeys;
+  private static readonly object p_addRemoveKeysLocker = new();
+  private static volatile int p_intLocker;
+  private static readonly Dictionary<string, KeyExt[]> p_keysSets = [];
+  private static readonly Dictionary<KeyExt, long?> p_lastPressed = [];
+
+  /// <summary>
+  ///     ATTENTION!
+  ///     <para>As this event is static, it raise with all registered Keys, from all identifiers</para>
+  /// </summary>
+  public static event Action<KeyExt>? KeyPressed;
+
+  static HotkeyManager()
+  {
+    p_uniqKeys = [];
+    p_timer.Elapsed += TimerOnElapsed;
+  }
+
+  /// <summary>
+  ///     Add Keys to listener
+  /// </summary>
+  /// <param name="_id">Unique identifier</param>
+  /// <param name="_keysToHandle">Set of Keys to listen to</param>
+  public static void AddKeys(string _id, params KeyExt[] _keysToHandle)
+  {
+    if (string.IsNullOrWhiteSpace(_id))
+      throw new ArgumentNullException(nameof(_id));
+    if (_keysToHandle == null || _keysToHandle.Length == 0)
+      throw new ArgumentNullException(nameof(_keysToHandle));
+
+    lock (p_addRemoveKeysLocker)
     {
-        private static readonly System.Timers.Timer p_timer = new(50);
-        private static KeyExt[] p_uniqKeys;
-        private static readonly object p_addRemoveKeysLocker = new();
-        private static volatile int p_intLocker;
-        private static readonly Dictionary<string, KeyExt[]> p_keysSets = new();
+      if (p_keysSets.Keys.Contains(_id))
+        throw new InvalidOperationException("ID is already present");
 
-        /// <summary>
-        ///     ATTENTION!
-        ///     <para>As this event is static, it raise with all registered Keys, from all identifiers</para>
-        /// </summary>
-        public static event Action<KeyExt> KeyPressed;
+      p_timer.Enabled = false;
+      while (p_intLocker != 0)
+        Thread.Sleep(1);
 
-        static HotkeyManager()
-        {
-            p_timer.Elapsed += TimerOnElapsed;
-        }
-
-        /// <summary>
-        ///     Add Keys to listener
-        /// </summary>
-        /// <param name="id">Unique identifier</param>
-        /// <param name="keysToHandle">Set of Keys to listen to</param>
-        public static void AddKeys(string id, params KeyExt[] keysToHandle)
-        {
-            if (string.IsNullOrWhiteSpace(id))
-                throw new ArgumentNullException(nameof(id));
-            if (keysToHandle == null || keysToHandle.Length == 0)
-                throw new ArgumentNullException(nameof(keysToHandle));
-
-            lock (p_addRemoveKeysLocker)
-            {
-                if (p_keysSets.Keys.Contains(id))
-                    throw new InvalidOperationException("ID is already present");
-
-                p_timer.Enabled = false;
-                while (p_intLocker != 0)
-                    Thread.Sleep(1);
-
-                p_keysSets[id] = keysToHandle;
-                RebuildUniqueKeys();
-                p_timer.Enabled = true;
-            }
-        }
-
-        /// <summary>
-        ///     Removes set of Keys with certain identifier from listener
-        /// </summary>
-        /// <returns>True if identifier is present, false otherwise</returns>
-        public static bool RemoveKeys(string id)
-        {
-            lock (p_addRemoveKeysLocker)
-            {
-                if (p_keysSets.ContainsKey(id))
-                {
-                    p_timer.Enabled = false;
-                    while (p_intLocker != 0)
-                    {
-                        Thread.Sleep(1);
-                    }
-                    p_keysSets.Remove(id);
-                    RebuildUniqueKeys();
-                    p_timer.Enabled = p_uniqKeys.Length != 0;
-                    return true;
-                }
-                return false;
-            }
-        }
-
-        private static void RebuildUniqueKeys()
-        {
-            var list = new List<KeyExt>();
-            foreach (KeyExt keyInfo in p_keysSets.Values.SelectMany(keys => keys))
-            {
-                if (!list.Any(l => l.Key == keyInfo.Key))
-                {
-                    list.Add(keyInfo);
-                }
-            }
-            p_uniqKeys = list.ToArray();
-        }
-
-        private static void TimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
-        {
-            if (p_intLocker != 0)
-                return;
-            Interlocked.Increment(ref p_intLocker);
-            try
-            {
-                int currentTime = Environment.TickCount;
-                bool alt = (NativeMethods.GetAsyncKeyState(Keys.Menu) & 0x8000) != 0;
-                bool shift = (NativeMethods.GetAsyncKeyState(Keys.ShiftKey) & 0x8000) != 0;
-                bool ctrl = (NativeMethods.GetAsyncKeyState(Keys.ControlKey) & 0x8000) != 0;
-                foreach (KeyExt keyExt in p_uniqKeys)
-                {
-                    bool pressed = (NativeMethods.GetAsyncKeyState(keyExt.Key) & 0x8000) != 0;
-                    if (pressed && (!keyExt.Pressed || currentTime - keyExt.PressedStartTime >= 500) && KeyPressed != null)
-                    {
-                        KeyPressed(new KeyExt(keyExt.Key, alt, shift, ctrl));
-                    }
-                    if (pressed && !keyExt.Pressed)
-                    {
-                        keyExt.PressedStartTime = currentTime;
-                    }
-                    keyExt.Pressed = pressed;
-                }
-            }
-            finally
-            {
-                Interlocked.Decrement(ref p_intLocker);
-            }
-        }
-        
+      p_keysSets[_id] = _keysToHandle;
+      RebuildUniqueKeys();
+      p_timer.Enabled = true;
     }
+  }
+
+  /// <summary>
+  ///     Removes set of Keys with certain identifier from listener
+  /// </summary>
+  /// <returns>True if identifier is present, false otherwise</returns>
+  public static bool RemoveKeys(string _id)
+  {
+    lock (p_addRemoveKeysLocker)
+    {
+      if (p_keysSets.ContainsKey(_id))
+      {
+        p_timer.Enabled = false;
+        while (p_intLocker != 0)
+          Thread.Sleep(1);
+
+        p_keysSets.Remove(_id);
+        RebuildUniqueKeys();
+        p_timer.Enabled = p_uniqKeys.Length != 0;
+        return true;
+      }
+      return false;
+    }
+  }
+
+  private static void RebuildUniqueKeys()
+  {
+    var list = new List<KeyExt>();
+    foreach (var keyInfo in p_keysSets.Values.SelectMany(_keys => _keys))
+      if (!list.Any(l => l.Key == keyInfo.Key))
+        list.Add(keyInfo);
+
+    p_uniqKeys = [.. list];
+  }
+
+  private static void TimerOnElapsed(object? _sender, ElapsedEventArgs _elapsedEventArgs)
+  {
+    if (p_intLocker != 0)
+      return;
+
+    Interlocked.Increment(ref p_intLocker);
+    try
+    {
+      var ticksMs = Environment.TickCount64;
+      var altPressed = (NativeMethods.GetAsyncKeyState(Keys.Menu) & 0x8000) != 0;
+      var shiftPressed = (NativeMethods.GetAsyncKeyState(Keys.ShiftKey) & 0x8000) != 0;
+      var ctrlPressed = (NativeMethods.GetAsyncKeyState(Keys.ControlKey) & 0x8000) != 0;
+      foreach (var keyExt in p_uniqKeys)
+      {
+        var keyPressed = (NativeMethods.GetAsyncKeyState(keyExt.Key) & 0x8000) != 0;
+        if (keyPressed)
+        {
+          var lastPressedMs = p_lastPressed.GetValueOrDefault(keyExt);
+          if (lastPressedMs == null || ticksMs - lastPressedMs.Value >= 500)
+            KeyPressed?.Invoke(new KeyExt(keyExt.Key, altPressed, shiftPressed, ctrlPressed));
+
+          if (lastPressedMs == null)
+            p_lastPressed[keyExt] = ticksMs;
+        }
+        else
+        {
+          p_lastPressed[keyExt] = null;
+        }
+      }
+    }
+    finally
+    {
+      Interlocked.Decrement(ref p_intLocker);
+    }
+  }
+
 }
